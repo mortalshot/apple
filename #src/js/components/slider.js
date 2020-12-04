04bacb3f505fed0dd2eeb6fef9fef33f310a16f0
mortalshot <seregagl1996@gmail.com>
2020-12-04 13:13:40 +0300

    product section

---
+++ #src/js/components/slider.js
@@ -0,0 +1,22 @@
+$('.gallery__main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    // adaptiveHeight: true,
+    asNavFor: '.gallery__thumbnails',
+});
+$('.gallery__thumbnails').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.gallery__main',
+    arrows: false,
+    focusOnSelect: true,
+    vertical: true,
+    verticalSwiping: true,
+});
+
+$('#productGallery .tabs-triggers__item').click(function() {
+    $('.gallery__main').slick('refresh');
+    $('.gallery__thumbnails').slick('refresh');
+})
--- #src/js/index.js
+++ #src/js/index.js
@@ -1,3 +1,9 @@
+// libs
+@@include('libs/slick.min.js')
+
+// components
 @@include('components/webp.js')
 @@include('components/menu.js')
+@@include('components/slider.js')
+@@include('components/tabs.js')
 
